5975582284349af31ab8ac9c45954c3e6fd8d9ac
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-10-08 11:06:48 +0200

    test 1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -13,7 +13,36 @@ import {
   UserName,
   Date,
 } from './BlogCard.styled';
+import { formatDistance } from 'helpers/formatDistance';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+export const BlogCard = ({
+  postedAt,
+  avatar,
+  userName,
+  description,
+  title,
+  tag,
+  poster,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={title} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt="Jane Doe" />
+          <UserInfo>
+            <UserName>{userName}</UserName>
+            <Date>{formatDistance(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
---
+++ src/helpers/formatDistance.js
@@ -0,0 +1,5 @@
+import { formatDistanceToNow } from 'date-fns';
+
+export const formatDistance = (data) => {
+    return formatDistanceToNow(new Date(data), { addSuffix: true })
+}
